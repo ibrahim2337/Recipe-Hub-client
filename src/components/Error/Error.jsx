f572ffdfab58f5a90ab76b198143d681c9f2e180
ibrahim2337 <ibrahimkhalilullah680@gmail.com>
2023-05-01 23:50:51 +0600

    project create successfully

---
+++ src/components/Error/Error.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Error;
